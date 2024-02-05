77f909007c8ef5290d978ada2e6aa9eaa08c1b35
Rodrigo <rodhoff@gmail.com>
2024-02-05 19:26:06 -0300

    Primeira Versão da API

--- escola/settings.py
+++ escola/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES' : (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAdminUser'
     )
 }
